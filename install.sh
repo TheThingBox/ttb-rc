ada06dca1787af4f259b7235ed7a086d704654a9
lucaspouchot <lucas.pouchot@gmail.com>
2019-07-02 14:54:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,16 @@
 cd /root
 
 # bashrc
-curl -h "https://gist.githubusercontent.com/lucaspouchot/fc42e4e18f687a4f57c30a293eb25a1d/raw/fa5212ab2dab95c6add692038a372dc86e4e2b45/.bashrc" > /root/.bashrc
+curl -h "https://raw.githubusercontent.com/TheThingBox/ttb-rc/master/.bashrc" > /root/.bashrc
 chmod 644 /root/.bashrc
 cp /root/.bashrc /home/pi/
 
 # vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git /root/.vim_runtime
 sh /root/.vim_runtime/install_awesome_vimrc.sh
-rm /root/.vim_runtime/.git* -r
+rm /root/.vim_runtime/.git* -R
+cp /root/.vimrc /home/pi/
+cp /root/.vim_runtime /home/pi/ -r
 
 # nanorc
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
